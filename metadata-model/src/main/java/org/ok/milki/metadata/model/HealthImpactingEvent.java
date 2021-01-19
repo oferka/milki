c542ab97ad97460043a63129d9a439a514fc4eac
ofer.karp <ofer.karp@gmail.com>
2021-01-19 20:01:22 +0200

    add HealthImpactingEvent to metadata-model module

---
+++ metadata-model/src/main/java/org/ok/milki/metadata/model/HealthImpactingEvent.java
@@ -0,0 +1,44 @@
+package org.ok.milki.metadata.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.ok.milki.model.utils.Identifiable;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Keyword;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Document(indexName = "milki_health_impacting_event")
+public class HealthImpactingEvent implements Identifiable {
+
+    @Id
+    @Getter
+    @NotNull
+    @Size(min = 2, max = 64)
+    @NotBlank
+    @Field(type = Keyword)
+    private String id;
+
+    @Getter
+    @NotNull
+    @Size(min = 2, max = 64)
+    @NotBlank
+    @Field(type = Keyword)
+    private String name;
+
+    @Getter
+    @NotNull
+    @Field(type = FieldType.Object)
+    private Product product;
+}
