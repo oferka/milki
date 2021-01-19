c974ac13a6304240259591af576e5a4181e77d82
ofer.karp <ofer.karp@gmail.com>
2021-01-19 13:42:27 +0200

    add Customer to metadata-model module

--- metadata-model/src/main/java/org/ok/milki/metadata/model/Customer.java
+++ metadata-model/src/main/java/org/ok/milki/metadata/model/Customer.java
@@ -8,7 +8,6 @@ import org.ok.milki.model.utils.Identifiable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -19,8 +18,8 @@ import static org.springframework.data.elasticsearch.annotations.FieldType.Keywo
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Document(indexName = "milki_account")
-public class Account implements Identifiable {
+@Document(indexName = "milki_customer")
+public class Customer implements Identifiable {
 
     @Id
     @Getter
@@ -36,9 +35,4 @@ public class Account implements Identifiable {
     @NotBlank
     @Field(type = Keyword)
     private String name;
-
-    @Getter
-    @NotNull
-    @Field(type = FieldType.Object)
-    private Customer customer;
 }
