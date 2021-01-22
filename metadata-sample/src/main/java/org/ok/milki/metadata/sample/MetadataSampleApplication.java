4c6e6a03970d324f8841409ed565239e246ca33c
ofer.karp <ofer.karp@gmail.com>
2021-01-22 07:20:11 +0200

    log customers

--- metadata-sample/src/main/java/org/ok/milki/metadata/sample/MetadataSampleApplication.java
+++ metadata-sample/src/main/java/org/ok/milki/metadata/sample/MetadataSampleApplication.java
@@ -1,5 +1,6 @@
 package org.ok.milki.metadata.sample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ok.milki.metadata.model.customer.Customer;
 import org.ok.milki.metadata.sample.customer.SampleCustomerProvider;
 import org.springframework.boot.CommandLineRunner;
@@ -7,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @SpringBootApplication
+@Slf4j
 public class MetadataSampleApplication {
 
     public static void main(String[] args) {
@@ -20,7 +23,13 @@ public class MetadataSampleApplication {
     public CommandLineRunner helloWorld(SampleCustomerProvider sampleCustomerProvider) {
         return args -> {
             List<Customer> customers = sampleCustomerProvider.getCustomers(3);
-            System.out.println(customers);
+            log(customers);
         };
+    }
+
+    private void log(@NotNull List<Customer> customers) {
+        for(Customer customer : customers) {
+            log.info(customer.toString());
+        }
     }
 }
