d351c768b321385df428b13579a62cc152f2804d
ofer.karp <ofer.karp@gmail.com>
2021-01-26 09:44:45 +0200

    add test for metadata controller

---
+++ metadata-server/src/test/java/org/ok/milki/metadata/server/controller/MetadataControllerTest.java
@@ -0,0 +1,38 @@
+package org.ok.milki.metadata.server.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import javax.validation.constraints.NotNull;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.ok.milki.metadata.model.MetadataPaths.METADATA_PATH;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MetadataControllerTest {
+
+    @Autowired
+    @NotNull
+    private MockMvc mvc;
+
+    @Test
+    void getMessage() throws Exception {
+        MvcResult mvcResult = mvc.perform(get(format("/%s", METADATA_PATH))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(log())
+                .andExpect(status().isOk())
+                .andReturn();
+        assertNotNull(mvcResult);
+    }
+}
