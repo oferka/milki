de8ad0e42eeab750cf6271abc4900f6b4e2520ae
ofer.karp <ofer.karp@gmail.com>
2021-01-30 23:35:32 +0200

    add metadata-ui

--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewHeader.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewHeader.java
@@ -1,12 +1,14 @@
 package org.ok.milki.metadata.ui.main;
 
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 import javax.validation.constraints.NotNull;
 
+@CssImport("./styles/views/main/main-view-header.css")
 public class MainViewHeader extends HorizontalLayout {
 
     private final DrawerToggle drawerToggle;
