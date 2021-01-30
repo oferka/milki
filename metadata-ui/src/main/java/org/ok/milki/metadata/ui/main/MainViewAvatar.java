89fef77570d90283b8f18dd778ace91bbddc42a1
ofer.karp <ofer.karp@gmail.com>
2021-01-30 23:37:39 +0200

    add metadata-ui

--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewAvatar.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewAvatar.java
@@ -1,7 +1,9 @@
 package org.ok.milki.metadata.ui.main;
 
 import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.dependency.CssImport;
 
+@CssImport("./styles/views/main/main-view-avatar.css")
 public class MainViewAvatar extends Avatar {
 
     public MainViewAvatar() {
