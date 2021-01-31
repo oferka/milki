ae7753660c0d7b08f3efe5e8990a917a19367874
ofer.karp <ofer.karp@gmail.com>
2021-01-31 09:06:21 +0200

    move drawer to separate class

--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainView.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainView.java
@@ -1,30 +1,17 @@
 package org.ok.milki.metadata.ui.main;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
-
-import java.util.Optional;
 
 @CssImport("./styles/views/main/main-view.css")
 @PWA(name = "Milki", shortName = "Milki", enableInstallPrompt = false)
 @JsModule("./styles/shared-styles.js")
 public class MainView extends AppLayout {
 
-    private final Tabs menu;
+    private final MainViewDrawer mainViewDrawer;
     private final MainViewHeader mainViewHeader;
 
     public MainView() {
@@ -33,59 +20,15 @@ public class MainView extends AppLayout {
         mainViewHeader = new MainViewHeader();
         addToNavbar(true, mainViewHeader);
 
-        menu = createMenu();
-        addToDrawer(createDrawerContent(menu));
-    }
-
-    private Tabs createMenu() {
-        final Tabs tabs = new Tabs();
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
-        tabs.setId("tabs");
-        tabs.add(createMenuItems());
-        return tabs;
-    }
-
-    private Component[] createMenuItems() {
-        return new Tab[] {
-                createTab("Hello World", HelloWorldView.class),
-                createTab("About", AboutView.class)
-        };
-    }
-
-    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
-        final Tab tab = new Tab();
-        tab.add(new RouterLink(text, navigationTarget));
-        ComponentUtil.setData(tab, Class.class, navigationTarget);
-        return tab;
-    }
-
-    private Component createDrawerContent(Tabs menu) {
-        VerticalLayout layout = new VerticalLayout();
-        layout.setSizeFull();
-        layout.setPadding(false);
-        layout.setSpacing(false);
-        layout.getThemeList().set("spacing-s", true);
-        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        HorizontalLayout logoLayout = new HorizontalLayout();
-        logoLayout.setId("logo");
-        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        logoLayout.add(new Image("images/logo.png", "My Project logo"));
-        logoLayout.add(new H1("My Project"));
-        layout.add(logoLayout, menu);
-        return layout;
+        mainViewDrawer = new MainViewDrawer();
+        addToDrawer(mainViewDrawer);
     }
 
     @Override
     protected void afterNavigation() {
         super.afterNavigation();
-        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+        mainViewDrawer.afterNavigation(getContent());
         mainViewHeader.setViewTitleText(getCurrentPageTitle());
-    }
-
-    private Optional<Tab> getTabForComponent(Component component) {
-        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
-                .findFirst().map(Tab.class::cast);
     }
 
     private String getCurrentPageTitle() {
---
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewDrawer.java
@@ -0,0 +1,73 @@
+package org.ok.milki.metadata.ui.main;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.router.RouterLink;
+
+import java.util.Optional;
+
+@CssImport("./styles/views/main/main-view-drawer.css")
+public class MainViewDrawer extends VerticalLayout {
+
+    private final Tabs menu;
+
+    public MainViewDrawer() {
+        setId("main-view-drawer");
+
+        setSizeFull();
+        setPadding(false);
+        setSpacing(false);
+        getThemeList().set("spacing-s", true);
+        setAlignItems(FlexComponent.Alignment.STRETCH);
+
+        menu = createMenu();
+
+        HorizontalLayout logoLayout = new HorizontalLayout();
+        logoLayout.setId("logo");
+        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        logoLayout.add(new Image("images/logo.png", "My Project logo"));
+        logoLayout.add(new H1("My Project"));
+
+        add(logoLayout, menu);
+    }
+
+    private Tabs createMenu() {
+        final Tabs tabs = new Tabs();
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
+        tabs.setId("tabs");
+        tabs.add(createMenuItems());
+        return tabs;
+    }
+
+    private Component[] createMenuItems() {
+        return new Tab[] {
+                createTab("Hello World", HelloWorldView.class),
+                createTab("About", AboutView.class)
+        };
+    }
+
+    private Tab createTab(String text, Class<? extends Component> navigationTarget) {
+        final Tab tab = new Tab();
+        tab.add(new RouterLink(text, navigationTarget));
+        ComponentUtil.setData(tab, Class.class, navigationTarget);
+        return tab;
+    }
+
+    public void afterNavigation(Component content) {
+        getTabForComponent(content).ifPresent(menu::setSelectedTab);
+    }
+
+    private Optional<Tab> getTabForComponent(Component content) {
+        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(content.getClass())).findFirst().map(Tab.class::cast);
+    }
+}
