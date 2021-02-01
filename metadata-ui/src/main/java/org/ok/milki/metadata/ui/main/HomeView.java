41957ee331e868487af36f1671fa1cdd6abb3d90
ofer.karp <ofer.karp@gmail.com>
2021-02-01 17:32:16 +0200

    add home view

--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/HomeView.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/HomeView.java
@@ -1,6 +1,8 @@
 package org.ok.milki.metadata.ui.main;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,5 +16,9 @@ public class HomeView extends HorizontalLayout {
 
     public HomeView() {
         setId("home-view");
+        setAlignItems(FlexComponent.Alignment.CENTER);
+
+        Text text = new Text("Home");
+        add(text);
     }
 }
