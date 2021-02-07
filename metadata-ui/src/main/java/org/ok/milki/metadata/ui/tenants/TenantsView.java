a63fc090c3c0d33f862db6d91018a1cafa2b0ff0
ofer.karp <ofer.karp@gmail.com>
2021-02-07 16:49:51 +0200

    add services view

--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/tenants/TenantsView.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/tenants/TenantsView.java
@@ -1,11 +1,10 @@
 package org.ok.milki.metadata.ui.tenants;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.ok.milki.metadata.ui.main.MainView;
@@ -15,7 +14,7 @@ import static org.ok.milki.metadata.ui.tenants.TenantsView.ROUTE;
 @Route(value = ROUTE, layout = MainView.class)
 @PageTitle(TenantsView.VIEW_NAME)
 @CssImport("./styles/views/tenants/tenants-view.css")
-public class TenantsView extends HorizontalLayout {
+public class TenantsView extends VerticalLayout {
 
     public static final String ROUTE = "tenants";
     public static final String VIEW_ID = "tenants-view";
@@ -26,6 +25,8 @@ public class TenantsView extends HorizontalLayout {
 
     public TenantsView() {
         setId(VIEW_ID);
+
+        setHeightFull();
         setAlignItems(FlexComponent.Alignment.CENTER);
 
         tenantsViewHeader = new TenantsViewHeader();
