f12fcb2f9e8e943af5a2e21aaa62520c22421c1e
ofer.karp <ofer.karp@gmail.com>
2021-02-20 07:47:29 +0200

    fix imports

--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewDrawer.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/main/MainViewDrawer.java
@@ -1,11 +1,9 @@
 package org.ok.milki.metadata.ui.main;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.tabs.Tabs;
 
 @CssImport("./styles/views/main/main-view-drawer.css")
 public class MainViewDrawer extends VerticalLayout {
@@ -25,10 +23,7 @@ public class MainViewDrawer extends VerticalLayout {
         mainViewTabs = new MainViewTabs();
         mainViewDrawerHeader = new MainViewDrawerHeader();
 
-        Text text = new Text("More Tabs");
-        Tabs moreTabs = new MainViewTabs();
-
-        add(mainViewDrawerHeader, mainViewTabs, text, moreTabs);
+        add(mainViewDrawerHeader, mainViewTabs);
     }
 
     public void selectTab(Component content) {
