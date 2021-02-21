34d44da498b0596c1b87a7e872977ecb21d7c899
ofer.karp <ofer.karp@gmail.com>
2021-02-21 22:41:08 +0200

    add tool view

---
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/tools/ToolView.java
@@ -0,0 +1,36 @@
+package org.ok.milki.metadata.ui.tools;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.ok.milki.metadata.ui.main.MainView;
+
+import static org.ok.milki.metadata.ui.tools.ToolView.VIEW_NAME;
+import static org.ok.milki.metadata.ui.tools.ToolsView.ROUTE;
+
+@Route(value = ROUTE, layout = MainView.class)
+@PageTitle(VIEW_NAME)
+@CssImport("./styles/views/"+ ROUTE + "/" + "tool-view.css")
+public class ToolView extends VerticalLayout implements HasUrlParameter<String> {
+
+    public static final String ROUTE = "tools";
+    public static final String VIEW_NAME = "Tool";
+    public static final String ENTITY_NAME = "Tool";
+
+    private final Text text;
+
+    public ToolView() {
+        setId("tool-view");
+
+        text = new Text("some initial value");
+    }
+
+    @Override
+    public void setParameter(BeforeEvent event, String parameter) {
+        text.setText(String.format("Hello, %s!", parameter));
+    }
+}
--- metadata-ui/src/main/java/org/ok/milki/metadata/ui/tools/ToolsViewBody.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/ui/tools/ToolsViewBody.java
@@ -1,17 +1,19 @@
 package org.ok.milki.metadata.ui.tools;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouterLink;
 
 @CssImport("./styles/views/tools/tools-view-body.css")
 public class ToolsViewBody extends VerticalLayout {
 
     public ToolsViewBody() {
         setId("tools-view-body");
-        Button tool1 = new Button("tool 1");
-        Button tool2 = new Button("tool 2");
-        Button tool3 = new Button("tool 3");
-        add(tool1, tool2, tool3);
+        RouterLink tool1 = new RouterLink("tool 1", ToolView.class, "1");
+        RouterLink tool2 = new RouterLink("tool 2", ToolView.class, "2");
+        RouterLink tool3 = new RouterLink("tool 3", ToolView.class, "3");
+        RouterLink tool4 = new RouterLink("tool 4", ToolView.class, "4");
+        RouterLink tool5 = new RouterLink("tool 5", ToolView.class, "5");
+        add(tool1, tool2, tool3, tool4, tool5);
     }
 }
