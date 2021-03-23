e44e52a9dba1841bd18e78dfb8bdc695ffcb4871
ofer.karp <ofer.karp@gmail.com>
2021-03-23 11:21:09 +0200

    2

--- metadata-ui/src/main/java/org/ok/milki/metadata/data/ClientEntityDataProvider.java
+++ metadata-ui/src/main/java/org/ok/milki/metadata/data/ClientEntityDataProvider.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import javax.validation.constraints.NotNull;
 
 import static java.lang.String.format;
-import static org.apache.commons.lang3.RandomUtils.nextInt;
 
 @Service
 @Primary
@@ -18,7 +17,18 @@ public class ClientEntityDataProvider implements EntityDataProvider {
     @Override
     public int count(@NotNull EntityType entityType) {
         log.info(format("Count for entity type: %s started", entityType.getEntityName()));
-        int result = nextInt(0, 52);
+        int result = 0;
+        switch (entityType) {
+            case ACCOUNT -> result = 5;
+            case EMPLOYEE -> result = 6;
+            case INTERVIEW -> result = 7;
+            case POSITION -> result = 8;
+            case PRODUCT -> result = 9;
+            case REPOSITORY -> result = 10;
+            case SERVICE -> result = 11;
+            case TEAM -> result = 12;
+            case TENANT -> result = 13;
+        }
         log.info(format("Count for entity type: %s completed. Result is: %s", entityType.getEntityName(), result));
         return result;
     }
